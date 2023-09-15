6bf2c4aeb02c3f036a23a15a665d4707b4e7ed5c
at-syot <aotsincere@gmail.com>
2023-09-15 11:40:39 -0400

    fix extra space at right screen, sidebar height same as content

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,16 @@
-import { PropsWithChildren } from "react";
+import {PropsWithChildren} from "react";
 import Hamburger from "@/src/components/hamburger";
 import Sidebar from "@/src/components/sidebar";
 
 type Props = { title: string; gradientCls: string };
 export default function Layout(props: PropsWithChildren<Props>) {
   function getContainerCls() {
-    return "h-full flex flex-col bg-gradient-to-b " + props.gradientCls;
+    return "relative h-full w-full overflow-hidden flex flex-col bg-gradient-to-b " + props.gradientCls;
   }
+  
 
   return (
-    <div id="container" className={getContainerCls()}>
+    <div className={getContainerCls()}>
       <nav className="flex flex-row p-6 justify-between items-center">
         <img src="images/logo-black.png" className="h-[69px] w-[79px]" />
         <Hamburger />
@@ -18,11 +19,13 @@ export default function Layout(props: PropsWithChildren<Props>) {
       <p className="w-[200px] h-[54px] bg-black text-white flex items-center justify-center rounded-r-3xl font-bold text-[19px] shrink-0">
         {props.title}
       </p>
-
+  
+      {/* content */}
       <div className="grow p-6">
         {props.children}
-        <Sidebar />
       </div>
+      
+      <Sidebar />
     </div>
   );
 }
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,30 +3,56 @@
 // import profileButton from "@assets/images/botton-profile.svg";
 // import expButton from "@assets/images/button-experiences.svg";
 
-import { useStore } from "@nanostores/react";
-import { $sidebarOpen, triggerSidebar } from "@/src/store/sidebar-ui";
+import {useStore} from "@nanostores/react";
+import {$sidebarOpen, triggerSidebar} from "@/src/store/sidebar-ui";
+import {useEffect, useRef} from "react";
 
 function getCls(open: boolean) {
   let cls =
-    "absolute top-0 left-0 h-full flex flex-col bg-black transition-[width] duration-200 ease-in";
+    "absolute top-0 bottom-0 left-0 h-full flex flex-col bg-black transition-[width] duration-200 ease-in";
   cls += open ? " " + "w-full" : " " + "w-0";
   return cls;
 }
 
 export default function () {
   const sidebarOpen = useStore($sidebarOpen);
+  const navRef = useRef<HTMLDivElement>(null)
+  const hrefButtonsRef = useRef<HTMLDivElement>(null)
+  
+  useEffect(() => {
+    const buttons = hrefButtonsRef.current
+    const nav = navRef.current
+    if (!buttons || !nav) return
+    
+    if (sidebarOpen) {
+      setTimeout(() => {
+        const navHeight = nav.clientHeight
+        const { innerWidth, innerHeight } = window
+        
+        buttons.style.opacity = '1'
+        buttons.style.width = innerWidth + 'px'
+        buttons.style.height = (innerHeight - navHeight) + 'px'
+      }, 200)
+      return
+    }
+    
+    buttons.style.opacity = '0'
+    buttons.style.height = '0px'
+    buttons.style.width = '0px'
+  }, [sidebarOpen])
+  
   return (
     <div className={getCls(sidebarOpen)}>
-      <nav className="text-right flex justify-between items-center p-6">
-        <img src="images/logo-white.svg" />
+      <nav className="text-right flex justify-between items-center p-6" ref={navRef}>
+        <img src="images/logo-white.svg"/>
         <a onClick={() => triggerSidebar(false)}>
-          <img src="images/icon-close.svg" className="h-[35px] w-[35px]" />
+          <img src="images/icon-close.svg" className="h-[35px] w-[35px]"/>
         </a>
       </nav>
-
-      <div className="grow flex flex-col gap-3 justify-center items-center">
+      
+      <div className="flex flex-col gap-3 justify-center items-center duration-100 transition-[opacity]" ref={hrefButtonsRef}>
         <a href="/">
-          <img src="images/botton-profile.svg" className="h-[96px] w-[198px]" />
+          <img src="images/botton-profile.svg" className="h-[96px] w-[198px]"/>
         </a>
         <a href="/experience">
           <img
