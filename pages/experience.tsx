54fd3dc44f4c4fcd2f4e377bbbc4ffea734486a5
at-syot <aotsincere@gmail.com>
2023-10-29 21:46:58 -0400

    add previous exp.

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -45,6 +45,50 @@ const experiences: Experience[] = [
     description: "Exciting project K-Plus in that time called Victoria, I joined as an iOS engineer. Slip page, home menu with user bank accounts and bit of setting feature are all my hand." +
       "<br />" +
       "<br />",
+  },
+  {
+    year: '2016-18',
+    at: "Work from anywhere",
+    city: "-",
+    country: "TH",
+    position: "Fulltime-freelance Developer",
+    description: "I transitioned from a full-time job to full-time freelancing to diversify my experience, moving beyond iOS development. I engaged in a variety of projects, particularly focusing on smaller to medium-sized ones. This allowed me to gain insights into my preferences and strengths, helping me better understand my career path.",
+   },
+  {
+    year: '2015',
+    at: "BananaCode",
+    city: "BKK",
+    country: "TH",
+    position: "iOS Developer",
+    description: "BananaCode want me to become one of team's member. Job: \"KLeasing\" i work is implement, code, fixing bug and publish new version to app store" +
+    "<br />" + 
+    "<br />" +
+    "For this company i work as full-time freelance"
+  },
+  {
+    year: '2014',
+    at: "True Digital Plus",
+    city: "BKK",
+    country: "TH",
+    position: "iOS Developer",
+    description: "For this company I am just contractor, but I actually work at True Digital Plus company site. This company hire me as a consultant and create Framework for them. True Digital Plus(let’s call TDP) need Framework for their customer which customer are game develop company something. Let’s me explain about TDP, TDP is a game online and offline publisher that it. " + 
+    "<br />" +
+    "<br />" +
+    "TDP Framework: make for game developer if developer want to join or publish game in TDP name. developer need to implement their game and plugin TDP Framework." +
+    "<br />" +
+    "<br />" +
+    "TDP Framework feature: authentication for user and developer and this Framework offer Inapp-purchase that’s all I know in phase 1"
+  },
+  {
+    year: '2013',
+    at: "PromptNow Company Limited",
+    city: "BKK",
+    country: "TH",
+    position: "iOS Developer",
+    description: "First job is a big project about Financial service that create for purple bank in Thailand. I did not start earlier. I came to do next for 1.3 phase that’s form phase 3.0 – 4.3 which I do both iPhone and iPad because that time hasn’t developer enough for it. Finally, I was done." +
+    "<br />" +
+    "<br />" +
+    "Mostly I do function call ATM/Branch that about searching ATM and Branch close your current-location and can search data for other city. This function will show data like a pin in your map. User can see detail for each data, Have enable AR function user can user this for seek ATM/Branch with camera device"
   }
 ]
 
