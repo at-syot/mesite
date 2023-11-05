8e55fa9951b19cf410be04dffc327314f9addbff
at-syot <aotsincere@gmail.com>
2023-11-04 22:58:43 -0400

    fix ProfileProfessionalSkill grid

--- src/components/ProfileProfessionalSkill.tsx
+++ src/components/ProfileProfessionalSkill.tsx
@@ -2,15 +2,12 @@ import { useScreenDevice } from "../useScreenDevice";
 
 
 export default function ProfileProfessionalSkill() {
-  const screenDevice = useScreenDevice()
-  function containerCls() {
-    const size = screenDevice === 'mobile' ? '_60px' : '_70px'
-    return `grid grid-rows-[repeat(7,_minmax(0,${size}))] grid-cols-[repeat(7,${size})]`
-  }
-
   return (
-    <div className={containerCls()}>
-      <div className="row-start-1 row-end-3 col-start-3 col-span-3 flex flex-col items-center justify-center">
+    <div className="grid grid-rows-[repeat(7,_60px)]
+    grid-cols-[repeat(7,_60px)] md:grid-rows-[repeat(7,_70px)]
+    md:grid-cols-[repeat(7,_70px)]">
+      <div className="row-start-1 row-end-3 col-start-3 col-span-3 flex
+      flex-col items-center justify-center">
         <SkillBox img="images/Icon-communication.svg" title="COMMUNICATION" />
       </div>
       <div className="row-start-3 row-span-2 col-span-2 flex flex-col items-center justify-center">
@@ -49,13 +46,10 @@ export default function ProfileProfessionalSkill() {
 }
 
 function SkillBox(props: { img: string; title: string }) {
-  const screenDevice = useScreenDevice()
-  const textSize = screenDevice === 'mobile' ? "9px" : "16px"
-
   return (
     <aside className="flex flex-col items-center justify-center">
       <img src={props.img} className="h-[50px] w-[50px]" />
-      <label className={`text-[${textSize}] font-semibold`}>{props.title}</label>
+      <label className="text-[9px] md:text-[16px] font-semibold">{props.title}</label>
     </aside>
   );
 }
