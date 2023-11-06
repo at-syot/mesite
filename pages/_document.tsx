f4442120042c3fa675e6dc8eabec69b7c7a63ea2
at-syot <aotsincere@gmail.com>
2023-11-06 11:35:21 -0500

    test preview appearance.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,14 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <head>
-        <meta name="viewport" content="width=device-width; initial-scale=1.0" />
-        <title>OatCoding</title>
+      <Head>
         <meta property="og:title" content="Oat Software Engineer - CA|TH" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://oatcoding.vercel.app/" />
         <meta property="og:image" content="images/preview_cover.png" />
+      </Head>
+      <head>
+        <meta name="viewport" content="width=device-width; initial-scale=1.0" />
+        <title>OatCoding</title>
       </head>
       <body>
         <Main />
